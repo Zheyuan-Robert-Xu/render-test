adf979ddea49896eb122fcbed14952381b4658ec
Zheyuan Xu <Zheyuan-Robert-Xu@users.noreply.github.com>
2023-05-07 17:04:41 +1000

    change host to 10000

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

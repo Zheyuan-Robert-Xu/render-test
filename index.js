04add13f75955a7c7e21ca8881e7672c4ec9221f
Zheyuan Xu <Zheyuan-Robert-Xu@users.noreply.github.com>
2023-05-07 16:39:41 +1000

    exercise 3.9-3.11

--- index.js
+++ index.js
@@ -1,99 +1,134 @@
 const express = require("express");
 const app = express();
+var morgan = require("morgan");
 const cors = require("cors");
-
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
 
 app.use(cors());
 app.use(express.json());
-app.use(requestLogger);
 app.use(express.static("build"));
+// app.use(morgan("tiny")); // is somewhat duplicated with the following code
 
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+app.get("/info", (request, response) => {
+  var numPerson = persons.length;
+  var date = new Date();
+  response.send(
+    `<div><p>Phonebook has info for ${numPerson} people</p><p>${date}</p></div>`
+  );
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body), //{"name":"Greate Robert","number":"058-2749539"}
+    ].join(" ");
+  })
+);
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// const unknownEndpoint = (req, res) => {
+//   res.status(404).send({ error: "unknown endpoint" });
+// };
 
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-
-  response.json(note);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-app.use(unknownEndpoint);
+// app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id); // id should be interget not string
+  const person = persons.find((p) => {
+    return p.id === id;
+  });
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+  if (persons.some((e) => e.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    number: body.number,
+    name: body.name,
+  };
+  persons = persons.concat(person);
+  response.json(person); // takes a Response stream and reads it to completion.
+  // it returns a promise which resolves with the result of parsing the body text as JSON
+  // the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
